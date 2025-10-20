699fb1b664110b2491d35e3cd6e39576e6a196fe
manup <magarn365@gmail.com>
2025-10-20 18:22:42 +0545

    docs: spelling correction

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>My Sample Page</title>
+      <title>My Sample Pages</title>
       <style>
         body {
           font-family: Arial, sans-serif;
